29ffcc723a12a8331c574f71074da9a8ee5b0f26
Md.Shamsuzzaman <dip.cse.nsu@gmail.com>
2022-03-04 17:33:55 +0600

    javascript and css file added

---
+++ index.js
@@ -0,0 +1,11 @@
+function show_alert(text_1) {
+    alert (text_1);
+}
+
+
+function zoom_s_icon(id){
+    document.getElementById(id).style.width="30px";
+}
+function zoom_s_icon_remove(id){
+    document.getElementById(id).style.width="24px";
+}
